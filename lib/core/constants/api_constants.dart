d69546ee17dacfdb609657f680269e9363099b55
Luiz Guilherme <163307645+lguimorais@users.noreply.github.com>
2026-05-07 10:58:19 -0300

    valores constantes da API

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+  static const String baseUrl = 'https://itunes.apple.com/search';
+
+  static const int defaultLimit = 20;
+  static const String defaultCountry = 'BR'; // resultado por padrao em PT_BR
+}
