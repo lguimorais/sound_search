661b46bd086cf390350ad3c1515582f0215d438d
Luiz Guilherme <163307645+lguimorais@users.noreply.github.com>
2026-05-07 11:46:29 -0300

    construtors Track model

--- lib/data/models/Track_model.dart
+++ lib/data/models/Track_model.dart
@@ -1,1 +1,17 @@
 import '../../domain/entities/track.dart';
+
+class TrackModel extends Track {
+  TrackModel({
+    required super.trackId,
+    required super.trackName,
+    required super.artistName,
+    required super.collectionName,
+    required super.previewUrl,
+    required super.artworkUrl,
+    required super.genre,
+    required super.trackTimeMillis,
+    required super.trackPrice,
+    required super.isExplicit,
+    super.suggestToRadio,
+  });
+}
